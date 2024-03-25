4c70d015a5645aae31532c362bf0db9a5a887aea
Nicky  shah <nickshah534@gmail.com>
2024-03-25 16:19:35 +1100

    axios added

--- src/Pages/Companies/Companies.page.tsx
+++ src/Pages/Companies/Companies.page.tsx
@@ -1,6 +1,19 @@
 import "./companies.scss";
+import httpModule from "../../helpers/http.module";
+import { useEffect, useState } from "react";
+import { ICompany } from "../../Types/global.typing";
+
 
 const Companies = () => {
+    const [companies, setCopmanies] = useState<ICompany[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+
+useEffect(()=>{
+    setLoading(true);
+    httpModule.get<ICompany[]>("/Company/Get")
+    .then(response => )
+    .catch()
+})
   return (
     <div>Companies.page</div>
   )
---
+++ src/Types/global.typing.ts
@@ -0,0 +1,13 @@
+export interface ICompany {
+    id:string;
+    name:string;
+    size:string;
+    createdAt:string;
+}
+
+// {
+//     "id": 2,
+//     "name": "Microsoft",
+//     "createdAt": "2024-03-19T16:14:07.9763472",
+//     "size": "Small"
+//   },

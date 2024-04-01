ca524a9e18f99c881af2d1730e1219b35f8c40a1
Nicky  shah <nickshah534@gmail.com>
2024-04-02 10:59:49 +1100

    added

--- src/Components/companies/companiesGrid.componenet.tsx
+++ src/Components/companies/companiesGrid.componenet.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import moment from "moment";
+import React from "react";
+import { ICompany } from "../../Types/global.typing";
 
-const companiesGrid = () => {
-  return (
-    <div className='companies.grid'>
-        
-    </div>
-  )
+const column: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 100 },
+  { field: "name", headerName: "Name", width: 200 },
+  { field: "size", headerName: "Size", width: 150 },
+  {
+    field: "createdAt",
+    headerName: "Creation Time",
+    width: 200,
+    renderCell: (params) => moment(params.row.createdAt).format("YYYY-MM-DD"),
+  },
+];
+
+interface ICompaniesGridprops {
+  data: ICompany[];
 }
 
-export default companiesGrid
+const CompaniesGrid = ({ data }: ICompaniesGridprops) => {
+  return (
+    <Box sx={{ width: "100%", height: 450 }} className="companies.grid">
+      <DataGrid
+        rows={data}
+        columns={column}
+        getRowId={(row) => row.id}
+        rowHeight={50}
+      />
+    </Box>
+  );
+};
+
+export default CompaniesGrid;
--- src/Pages/Companies/Companies.page.tsx
+++ src/Pages/Companies/Companies.page.tsx
@@ -2,9 +2,10 @@ import { useEffect, useState } from "react";
 import "./companies.scss";
 import httpModule from "../../helpers/http.module";
 import { ICompany } from "../../Types/global.typing";
-import { Button } from "@mui/material";
+import { Button, CircularProgress } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { Add } from "@mui/icons-material";
+import CompaniesGrid from "../../Components/companies/CompaniesGrid.componenet";
 
 const Companies = () => {
   const [companies, setCompanies] = useState<ICompany[]>([]);
@@ -35,7 +36,13 @@ const Companies = () => {
           <Add />
         </Button>
       </div>
-      
+      {loading ? (
+        <CircularProgress size={100} />
+      ) : companies.length === 0 ? (
+        <h1>No Company</h1>
+      ) : (
+        <CompaniesGrid data={companies} />
+      )}
     </div>
   );
 };
